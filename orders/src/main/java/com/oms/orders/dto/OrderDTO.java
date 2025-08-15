83456de4643de4980f38d0d8b4258def4b6d62aa
abhitatachar2000 <abhishektatachar@gmail.com>
2025-08-15 17:44:00 +0530

    adds controller for order module

---
+++ orders/src/main/java/com/oms/orders/dto/OrderDTO.java
@@ -0,0 +1,90 @@
+package com.oms.orders.dto;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+public class OrderDTO {
+
+    private int id;
+
+    private int itemId;
+
+    private int quantity;
+
+    private double pricePerUnit;
+
+    private double totalPrice;
+
+    private String status;
+
+    private String contact;
+
+    public OrderDTO() {}
+
+    public OrderDTO(int itemId, int quantity, double pricePerUnit, double totalPrice, String status, String contact) {
+        this.itemId = itemId;
+        this.quantity = quantity;
+        this.pricePerUnit = pricePerUnit;
+        this.totalPrice = totalPrice;
+        this.status = status;
+        this.contact = contact;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int Id) {
+        this.id = Id;
+    }
+
+    public int getItemId() {
+        return itemId;
+    }
+
+    public void setItemId(int itemId) {
+        this.itemId = itemId;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public double getPricePerUnit() {
+        return pricePerUnit;
+    }
+
+    public void setPricePerUnit(double pricePerUnit) {
+        this.pricePerUnit = pricePerUnit;
+    }
+
+    public double getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(double totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getContact() {
+        return contact;
+    }
+
+    public void setContact(String contact) {
+        this.contact = contact;
+    }
+}
---
+++ orders/src/test/java/com/oms/orders/controller/OrdersControllerTest.java
@@ -0,0 +1,260 @@
+package com.oms.orders.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.oms.orders.dto.OrderDTO;
+import com.oms.orders.entity.OrderEntity;
+import com.oms.orders.entity.OrderStatus;
+import com.oms.orders.service.OrdersService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(OrdersController.class)
+@TestPropertySource(locations = "classpath:/application-test.properties")
+public class OrdersControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    private ObjectMapper objectMapper = new ObjectMapper();
+
+    @MockitoBean
+    private OrdersService ordersService;
+
+    @Test
+    void return201OnNewOrderCreation() throws Exception {
+        OrderDTO orderDTO = new OrderDTO(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+
+        OrderEntity orderEntity = new OrderEntity(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+        orderEntity.setId(1);
+
+        Mockito.doReturn(orderEntity).when(ordersService).createNewOrder(any(OrderEntity.class));
+        String jsonString = objectMapper.writeValueAsString(orderDTO);
+        mockMvc.perform(post("/api/v1/orders").contentType(MediaType.APPLICATION_JSON).content(jsonString))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    void returnInternalServerErrorIfAddingNewOrderFails() throws Exception {
+        OrderDTO orderDTO = new OrderDTO(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+
+        Mockito.doThrow(RuntimeException.class).when(ordersService).createNewOrder(any(OrderEntity.class));
+        String jsonString = objectMapper.writeValueAsString(orderDTO);
+        mockMvc.perform(post("/api/v1/orders").contentType(MediaType.APPLICATION_JSON).content(jsonString))
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    void successfullyFetchesAllExistingOrders() throws Exception {
+        OrderEntity orderOne = new OrderEntity(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+        orderOne.setId(1);
+
+        OrderEntity orderTwo = new OrderEntity(1,
+                10,
+                30d,
+                300d,
+                OrderStatus.DELIVERED,
+                "testNew@example.com");
+        orderTwo.setId(2);
+
+        Mockito.doReturn(Arrays.asList(orderOne, orderTwo)).when(ordersService).getAllOrders();
+
+        mockMvc.perform(get("/api/v1/orders"))
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[1].id").value(2));
+    }
+
+    @Test
+    void returnsInternalServerErrorIfFetchingItemsFails() throws Exception {
+        Mockito.doThrow(RuntimeException.class).when(ordersService).getAllOrders();
+        mockMvc.perform(get("/api/v1/orders")).andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    void findsItemWithIdIfExists() throws Exception {
+        OrderEntity orderOne = new OrderEntity(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+        orderOne.setId(1);
+        Mockito.doReturn(orderOne).when(ordersService).getOrderById(1);
+
+        mockMvc.perform(get("/api/v1/orders/1"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(1));
+    }
+
+    @Test
+    void returnsNotFoundIfOrderWithIdIsNotFound() throws Exception {
+        Mockito.doReturn(null).when(ordersService).getOrderById(1);
+        mockMvc.perform(get("/api/v1/orders/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void returnsInternalServerErrorIfFindingElementByIdFails() throws Exception {
+        Mockito.doThrow(RuntimeException.class).when(ordersService).getOrderById(1);
+        mockMvc.perform(get("/api/v1/orders/1"))
+                .andExpect(status().isInternalServerError());
+    }
+
+    @Test
+    void sucessfullyUpdatesTheExistingOrder() throws Exception {
+        OrderDTO orderDTO = new OrderDTO(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+
+        OrderEntity orderEntity = new OrderEntity(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+        orderEntity.setId(1);
+
+        Mockito.doReturn(orderEntity).when(ordersService).updateOrder(eq(1), any(OrderEntity.class));
+        mockMvc.perform(put("/api/v1/orders/1").contentType(MediaType.APPLICATION_JSON)
+                    .content(objectMapper.writeValueAsString(orderDTO)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.itemId").value(1))
+                .andExpect(jsonPath("$.quantity").value(10));
+
+    }
+
+    @Test
+    void returnsNotFoundIfOrderWithIdDoesNotExist() throws Exception {
+        OrderDTO orderDTO = new OrderDTO(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+        Mockito.doReturn(null).when(ordersService).updateOrder(eq(1), any(OrderEntity.class));
+        mockMvc.perform(put("/api/v1/orders/1").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(orderDTO)))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void throwsInternalServerErrorIfUpdatingOrderFails() throws Exception {
+        OrderDTO orderDTO = new OrderDTO(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+        Mockito.doThrow(RuntimeException.class).when(ordersService).updateOrder(eq(1), any(OrderEntity.class));
+        mockMvc.perform(put("/api/v1/orders/1").contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(orderDTO)))
+                .andExpect(status().isInternalServerError());
+
+    }
+
+    @Test
+    void sucessfullyDeletesTheExistingOrder() throws Exception {
+
+        Mockito.doReturn(true).when(ordersService).deleteOrderById(1);
+        mockMvc.perform(delete("/api/v1/orders/1"))
+                .andExpect(status().isOk());
+
+    }
+
+    @Test
+    void returnsNotFoundIfDeletingNonExistingOrder() throws Exception {
+        Mockito.doReturn(false).when(ordersService).deleteOrderById(1);
+        mockMvc.perform(delete("/api/v1/orders/1"))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    void throwsInternalServerErrorIfDeletingOrderFails() throws Exception {
+        Mockito.doThrow(RuntimeException.class).when(ordersService).deleteOrderById(1);
+        mockMvc.perform(delete("/api/v1/orders/1"))
+                .andExpect(status().isInternalServerError());
+
+    }
+
+    @Test
+    void sucessfullyFindsAllOrdersWithSpecifcStatus() throws Exception {
+        OrderEntity orderOne = new OrderEntity(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "test@example.com");
+
+        OrderEntity orderTwo = new OrderEntity(1,
+                10,
+                20d,
+                200d,
+                OrderStatus.NEW,
+                "testTwo@example.com");
+
+        Mockito.doReturn(Arrays.asList(orderOne, orderTwo)).when(ordersService).findAllOrderByStatus(OrderStatus.NEW);
+
+        mockMvc.perform(get("/api/v1/orders?status=new"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(2))
+                .andExpect(jsonPath("$[0].status").value("new"))
+                .andExpect(jsonPath("$[1].status").value("new"));
+    }
+
+    @Test
+    void returnsEmptyListWhenWhenNoOrdersOfStatusExists() throws Exception {
+        Mockito.doReturn(new ArrayList<OrderEntity>()).when(ordersService).findAllOrderByStatus(OrderStatus.NEW);
+        mockMvc.perform(get("/api/v1/orders?status=new"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.length()").value(0));
+    }
+
+    @Test
+    void returnsInternalServerErrorWhenFetchingOrderByStatusFails() throws Exception {
+        Mockito.doThrow(RuntimeException.class).when(ordersService).findAllOrderByStatus(OrderStatus.NEW);
+        mockMvc.perform(get("/api/v1/orders?status=new"))
+                .andExpect(status().isInternalServerError());
+    }
+}
